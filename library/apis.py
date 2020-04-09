410fcc26d7871fae6d2dbcc9ecd949f4370ac21b
Elias S. S. Vallejos <eliasssv@hotmail.com>
2020-04-08 21:52:10 -0300

    filter author

--- library/apis.py
+++ library/apis.py
@@ -5,7 +5,7 @@ from .serializers import AuthorSerializer, BookSerializer
 
 class AuthorViewSet(viewsets.ModelViewSet):
     """
-    ViewSet of Author
+    API of Author
     @since 2020-04-08
     @author eliasssv
     """
@@ -13,9 +13,21 @@ class AuthorViewSet(viewsets.ModelViewSet):
     serializer_class = AuthorSerializer
     permission_classes = [permissions.IsAuthenticated]
 
+    def get_queryset(self):
+        """
+        Optionally restricts the returned list containing the name of the Author 
+        @parameters: name (string)
+        @since: 2020-04-08
+        @author: eliasssv
+        """
+        name = self.request.query_params.get('name', None)
+        if name is not None:
+            self.queryset = self.queryset.filter(name__icontains=name)
+        return self.queryset
+
 class BookViewSet(viewsets.ModelViewSet):
     """
-    ViewSet of Book
+    API of Book
     @since 2020-04-08
     @author eliasssv
     """
