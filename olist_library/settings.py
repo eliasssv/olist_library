6bb60632988193c61c568403fd3fcbc7b1815530
Elias S. S. Vallejos <eliasssv@hotmail.com>
2020-04-11 21:15:12 -0300

    heroku

--- olist_library/settings.py
+++ olist_library/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", default=True, cast=config.boolean)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
