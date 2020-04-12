6084ae85419b4a402feb5c7ed2c2ae0d5ccad2e6
Elias S. S. Vallejos <eliasssv@hotmail.com>
2020-04-11 21:11:04 -0300

    heroku

--- olist_library/settings.py
+++ olist_library/settings.py
@@ -22,7 +22,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config("SECRET_KEY")
 
