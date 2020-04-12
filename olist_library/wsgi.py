ff34e8e39952184494a9c2ecb00dff11a6197147
Elias S. S. Vallejos <eliasssv@hotmail.com>
2020-04-11 21:25:15 -0300

    heroku

--- olist_library/wsgi.py
+++ olist_library/wsgi.py
@@ -1,16 +1,17 @@
 """
-WSGI config for olist_library project.
+WSGI config for cursodjango project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
 
+from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'olist_library.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "olist_library.settings")
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
