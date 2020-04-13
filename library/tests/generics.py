4c277793869af4be3c4d558c545d98877bdc9e61
Elias S. S. Vallejos <eliasssv@hotmail.com>
2020-04-13 15:37:34 -0300

    tests

---
+++ library/tests/generics.py
@@ -0,0 +1,46 @@
+from django.db.models import ObjectDoesNotExist
+
+### GENERICS METHODS TO TESTS ### 
+
+def generic_setUp(objects_to_create):
+    """
+    Generic setUp
+    @param objects_to_create : List of objects to save in database
+    @since 2020-04-13
+    @author eliasssv
+    """
+    for obj in objects_to_create:
+        obj.save()
+
+def generic_get(self, model, objects_to_get):
+    """
+    Generic Get
+    @param model : Class of the model
+    @param objects_to_create : List dict with:
+        {
+            search_field: name of the field search,
+            search_value: value of the search field,
+            compare_field: name of the field to compare,
+            compare_value: value of the comparation,
+        }
+    @since 2020-04-13
+    @author eliasssv
+    """
+    for obj in objects_to_get:
+        kwargs = {}
+        kwargs[obj['search_field']] = obj['search_value']
+        db_obj = model.objects.get(**kwargs)
+        self.assertEqual(eval(f"db_obj.{obj['compare_field']}"), obj['compare_value'])
+
+def generic_delete(self, model, objects_to_delete):
+    """
+    Generic Delete
+    @param objects_to_delete : List of objects to delete in database
+    @since 2020-04-13
+    @author eliasssv
+    """
+    for obj in objects_to_delete:
+        obj.delete()
+        with self.assertRaisesMessage(ObjectDoesNotExist,f'{type(obj).__name__} matching query does not exist.'):
+            model.objects.get(id=obj.id)
+
--- library/tests/test_models.py
+++ library/tests/test_models.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from ..models import Author, Book
-from django.db.models import ObjectDoesNotExist
+from .generics import generic_setUp, generic_get, generic_delete
 
 class AuthorTest(TestCase):
     """
@@ -9,14 +9,17 @@ class AuthorTest(TestCase):
     @author eliasssv
     """
     def setUp(self):
-        Author.objects.create(name='API TEST 1')
-        Author.objects.create(name='API TEST 2')
+        generic_setUp([Author(name='API TEST 1'), Author(name='API TEST 2')])
 
     def test_get(self):
-        author_1 = Author.objects.get(name='API TEST 1')
-        author_2 = Author.objects.get(name='API TEST 2')
-        self.assertEqual(author_1.name, "API TEST 1")
-        self.assertEqual(author_2.name, "API TEST 2")
+        generic_get(self, Author, [
+            {
+                "search_field": "name",
+                "search_value": "API TEST 1",
+                "compare_field": "name",
+                "compare_value":  "API TEST 1",
+            }
+        ])
 
     def test_update(self):
         author_1 = Author.objects.get(name='API TEST 1')
@@ -26,28 +29,33 @@ class AuthorTest(TestCase):
         self.assertEqual(author_1.name, "API TEST 1 UPDATED")
 
     def test_delete(self):
-        author_2 = Author.objects.get(name='API TEST 2')
-        author_2.delete()
-        with self.assertRaisesMessage(ObjectDoesNotExist,'Author matching query does not exist.'):
-            Author.objects.get(name='API TEST 2')
+        generic_delete(self, Author, [Author.objects.get(name='API TEST 2')])
 
-"""class BookTest(TestCase):
-
+class BookTest(TestCase):
+    """
+    Tests Model of Book
+    @since 2020-04-13
+    @author eliasssv
+    """
     def setUp(self):
         author_1 = Author.objects.create(name='API TEST 1')
         author_2 = Author.objects.create(name='API TEST 2')
-        Book.objects.create(
+        book_1 = Book.objects.create(
             name='API TEST 1',
             publication_year=2020,
-            edition=1,
-            authors=[author_1,author_2] 
+            edition=1
         )
-        Book.objects.create(
+        book_1.authors.set([author_1, author_2])
+
+        book_2 = Book.objects.create(
             name='API TEST 2',
             publication_year=1900,
-            edition=2,
-            authors=[author_2] 
+            edition=145
         )
+        book_2.authors.set([author_2])
+
+        generic_setUp([book_1, book_2])
+
 
     def test_get(self):
         book_1 = Book.objects.get(name='API TEST 1')
@@ -59,12 +67,10 @@ class AuthorTest(TestCase):
         book_1 = Book.objects.get(name='API TEST 1')
         book_1.name = "API TEST 1 UPDATED"
         book_1.save()
-        book_1 = Author.objects.get(name='API TEST 1 UPDATED')
+        book_1 = Book.objects.get(name='API TEST 1 UPDATED')
         self.assertEqual(book_1.name, "API TEST 1 UPDATED")
 
     def test_delete(self):
-        book_2 = Book.objects.get(name='API TEST 2')
-        book_2.delete()
-        with self.assertRaisesMessage(self.model.DoesNotExist):
-            Book.objects.get(name='API TEST 2')
-            """
+        generic_delete(self, Book, [Book.objects.get(name='API TEST 2')])
+
+    
